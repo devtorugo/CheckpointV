9ef84176825abe44be9a1af87210355f92f08e2c
VICTOR <victorzarbesu@gmail.com>
2024-05-02 14:56:09 -0300

    Criando componente Header

---
+++ app/components/Header/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './styles.css';
+import Link from "next/link";
+ 
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo">
+                <img src="ifood-43.png" alt="logo ifood" width={100} height={70}/>
+            </div>
+            <nav className="nav">
+                <ul>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Contato</a></li>
+                    <li><a href="#">Sobre nós</a></li>
+                
+                </ul>
+            </nav>
+            <div className="content">
+                <div className="buscador">
+                    <input type="text" placeholder="Buscar serviços..." />
+                </div>
+                <div className="botoes">
+        
+                    <Link href={'/caastrar'}><button className="botao-cadastrar">Cadastrar</button></Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+ 
+export default Header;
+ 
